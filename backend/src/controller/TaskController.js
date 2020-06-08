17aedb37ae5736dcd01a42f1a6c5cc51441dff12
pikeno221 <gabriellgomesbh@gmail.com>
2020-06-08 18:52:15 -0300

    filters by dates and find overdue tasks

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,6 @@
-const TaskModel = require('../model/TaskModel')
-
+const TaskModel = require('../model/TaskModel');
+const { startOfDay, endOfDay, startOfWeek, endOfWeek, startOfMonth, endOfMonth, startOfYear, endOfYear } = require('date-fns');
+const current = new Date();
 
 class TaskController {
 
@@ -13,40 +14,89 @@ class TaskController {
     async update(req, res) {
         await TaskModel.findByIdAndUpdate({ '_id': req.params.id }, req.body, { new: true })
             .then(response => { return res.status(200).json(response); })
-            .catch(error => { return res.status(422).json(error); })
+            .catch(error => { return res.status(422).json(error); });
     }
 
     async findAll(req, res) {
         await TaskModel.find({ 'macaddress': { '$in': req.body.macaddress } })
             .sort('when')
             .then(response => { return res.status(200).json(response); })
-            .catch(error => { return res.status(422).json(error); })
+            .catch(error => { return res.status(422).json(error); });
     }
 
 
     async findById(req, res) {
         await TaskModel.findById(req.params.id)
-            .then(response => { 
-                if (response) 
+            .then(response => {
+                if (response)
                     return res.status(200).json(response);
-                else 
-                    return res.status(204).json(null); })
-            .catch(error => { return res.status(422).json(error); })
+                else
+                    return res.status(204).json(null);
+            })
+            .catch(error => { return res.status(422).json(error); });
     }
 
 
     async delete(req, res) {
         await TaskModel.deleteOne({ '_id': req.params.id })
             .then(respose => { return res.status(204).json(null) })
-            .catch(error => { return res.status(422).json(error); })
+            .catch(error => { return res.status(422).json(error); });
     }
 
     async updateStatus(req, res) {
-        console.log(req.header('status'))
         await TaskModel.findByIdAndUpdate({ '_id': req.params.id }, { 'done': req.header('status') }, { new: true })
-            .then(response => { return res.status(200).json(response);})
+            .then(response => { return res.status(200).json(response); })
+            .catch(error => { return res.status(422).json(error); });
+    }
+
+    async findOverdue(req, res) {
+        await TaskModel.find({ 'when': { '$lte': current }, 'macaddress': { '$in': req.body.macaddress } })
+            .sort('when')
+            .then(response => { return res.status(200).json(response); })
+            .catch(error => { return res.status(422).json(error); });
+    }
+
+    async findToday(req, res) {
+        await TaskModel.find({
+            'when': { '$gte': startOfDay(current), '$lte': endOfDay(current) },
+            'macaddress': { '$in': req.body.macaddress }
+        })
+            .sort('when')
+            .then(response => { return res.status(200).json(response); })
+            .catch(error => { return res.status(422).json(error); });
+    }
+
+    async findWeek(req, res) {
+        await TaskModel.find({
+            'when': { '$gte': startOfWeek(current), '$lte': endOfWeek(current) },
+            'macaddress': { '$in': req.body.macaddress }
+        })
+            .sort('when')
+            .then(response => { return res.status(200).json(response); })
+            .catch(error => { return res.status(422).json(error); });
+    }
+
+    async findMonth(req, res) {
+        await TaskModel.find({
+            'when': { '$gte': startOfMonth(current), '$lte': endOfMonth(current) },
+            'macaddress': { '$in': req.body.macaddress }
+        })
+            .sort('when')
+            .then(response => { return res.status(200).json(response); })
             .catch(error => { return res.status(422).json(error); })
     }
+
+    async findYear(req, res) {
+        await TaskModel.find({
+            'when': { '$gte': startOfYear(current), '$lte': endOfYear(current) },
+            'macaddress': { '$in': req.body.macaddress }
+        })
+            .sort('when')
+            .then(response => { return res.status(200).json(response); })
+            .catch(error => { return res.status(422).json(error); });
+    }
+
+
 }
 
 
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -5,12 +5,19 @@ const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
 const MacAddressValidation = require('../middlewares/MacAddressValidation');
 
-router.post('/', TaskValidation, TaskController.create);
-router.put('/:id', TaskController.update);
+router.get('/today', MacAddressValidation, TaskController.findToday);
+router.get('/week', MacAddressValidation, TaskController.findWeek);
+router.get('/month', MacAddressValidation, TaskController.findMonth);
+router.get('/year', MacAddressValidation, TaskController.findMonth);
+router.get('/overdue', MacAddressValidation, TaskController.findOverdue);
 router.get('/', MacAddressValidation, TaskController.findAll);
 router.get('/:id', TaskController.findById);
-router.delete('/:id', TaskController.delete);
-router.put('/status/:id', TaskController.updateStatus);
 
+router.post('/', TaskValidation, TaskController.create);
+
+router.put('/status/:id', TaskController.updateStatus);
+router.put('/:id', TaskController.update);
+
+router.delete('/:id', TaskController.delete);
 
 module.exports = router;
